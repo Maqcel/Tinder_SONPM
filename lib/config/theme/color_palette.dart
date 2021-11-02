2b51f716ead8ff88d6af14196d9964d5b314f56c
Hubert Gielecinski <gielaktv@gmail.com>
2021-11-02 20:09:36 +0100

    added colors and font

---
+++ lib/config/theme/color_palette.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class ColorPalette {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF000000);
+  static const Color grayLight = Color(0xFFA6A6A6);
+  static const Color grayMid = Color(0xFF6E6E6E);
+  static const Color gray = Color(0xFF404040);
+  static const Color colorPrimary100 = Color(0xFFFF8993);
+  static const Color colorPrimary200 = Color(0xFFFF7983);
+  static const Color colorPrimary300 = Color(0xFFFF6874);
+  static const Color colorPrimaryBase = Color(0xFFFF5764);
+  static const Color colorPrimary400 = Color(0xFFE64E5A);
+  static const Color colorPrimary500 = Color(0xFFCC4650);
+  static const Color colorPrimary600 = Color(0xFFB33D46);
+  static const Color gold = Color(0xFFDDA650);
+  static const Color goldLight = Color(0xFFEDCF83);
+  static const Color redLight = Color(0xFFF07362);
+  static const Color red = Color(0xFFF3576D);
+  static const Color blueLight = Color(0xFF6CDAF2);
+  static const Color blue = Color(0xFF448DF1);
+  static const Color greenLight = Color(0xFF77EFE0);
+  static const Color green = Color(0xFF6BE2AB);
+  static const Color purpleLight = Color(0xFFB76BF8);
+  static const Color purple = Color(0xFF9343D5);
+}
