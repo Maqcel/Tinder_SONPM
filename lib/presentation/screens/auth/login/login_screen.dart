3e5c243db64f74694ce48c39c71d77dcf03ace80
Mikolaj Marcoll <mikolaj.marcoll@htdevelopers.com>
2021-11-03 11:37:57 +0100

    changes after code review

--- lib/presentation/screens/auth/login/login_screen.dart
+++ lib/presentation/screens/auth/login/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tinder/extensions/build_context_extension.dart';
+import 'package:tinder/gen/assets.gen.dart';
 import 'package:tinder/presentation/app/navigation/cubit/user_session_navigation_cubit.dart';
 import 'package:tinder/presentation/screens/auth/login/cubit/login_screen_cubit.dart';
 
@@ -40,58 +41,68 @@ class _LoginScreenState extends State<LoginScreen> {
     ),
   );
 
-  Widget _body(LoginScreenState state) => GestureDetector(
-        onTap: _onLoginButtonClicked,
-        child: Scaffold(
-          backgroundColor: const Color.fromRGBO(255, 87, 100, 1),
-          body: Column(
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 20),
-                alignment: Alignment.center,
-                child: Row(
-                  children: [
-                    // TODO: why image is not working
-                    // const Image(image: AssetImage('assets/images/icons/tinder_white.svg')),
-                    Container(
-                      margin: const EdgeInsets.only(left: 100, top: 200),
-                      child: const Icon(Icons.settings_display_sharp,
-                          color: Colors.white, size: 55),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 10, top: 200),
-                      child: const Text(
-                        "tinder",
-                        style: TextStyle(fontSize: 55, color: Colors.white),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(top: 120, bottom: 35),
-                child: Text(
-                  context.localizations.policyText,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(bottom: 20, right: 10, left: 10),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text(context.localizations.createAccountButton),
-                  style: createAccountButtonStyles,
-                ),
-              ),
-              Container(
-                margin: const EdgeInsets.only(bottom: 20, right: 10, left: 10),
-                child: OutlinedButton(
-                    onPressed: _onLoginButtonClicked,
-                    child: Text(context.localizations.signInButton),
-                    style: signInButtonStyles),
-              ),
-            ],
+  Widget _title() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 200),
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 100),
+            child: Assets.images.icons.tinderWhite
+                .svg(color: Colors.white, width: 75, height: 75),
           ),
+          Container(
+            margin: const EdgeInsets.only(left: 10),
+            child: Text(
+              context.localizations.appName,
+              style: const TextStyle(fontSize: 55, color: Colors.white),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _policy() {
+    return (Container(
+      margin: const EdgeInsets.only(top: 120, bottom: 35),
+      child: Text(
+        context.localizations.policyText,
+        style: const TextStyle(color: Colors.white),
+      ),
+    ));
+  }
+
+  Widget _createAccountButton() {
+    return (Container(
+      margin: const EdgeInsets.only(bottom: 20, right: 10, left: 10),
+      child: ElevatedButton(
+        onPressed: () {},
+        child: Text(context.localizations.createAccountButton),
+        style: createAccountButtonStyles,
+      ),
+    ));
+  }
+
+  Widget _signInButton() {
+    return (Container(
+      margin: const EdgeInsets.only(bottom: 20, right: 10, left: 10),
+      child: OutlinedButton(
+          onPressed: _onLoginButtonClicked,
+          child: Text(context.localizations.signInButton),
+          style: signInButtonStyles),
+    ));
+  }
+
+  Widget _body(LoginScreenState state) => Scaffold(
+        backgroundColor: const Color.fromRGBO(255, 87, 100, 1),
+        body: Column(
+          children: [
+            _title(),
+            _policy(),
+            _createAccountButton(),
+            _signInButton(),
+          ],
         ),
       );
 
